9571654b5995e36697aa014c619210ab3b4b5fe6
jeffpersonified <jeff.matthew.smith@gmail.com>
2015-06-07 21:35:31 -0700

    Finish all required

--- facebook_codepath/PhotoViewController.swift
+++ facebook_codepath/PhotoViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UIScrollViewDelegate  {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     var image: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
         
         self.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         imageView.image = image
@@ -28,6 +30,27 @@ class PhotoViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
 
+    func scrollViewDidScroll(scrollView: UIScrollView!) {
+        
+        var offset = Float(abs(scrollView.contentOffset.y))
+        var alpha = CGFloat(1 - (offset * 1.25 / 100))
+        self.view.alpha = alpha
+        scrollView.backgroundColor = UIColor(white: 0, alpha: alpha)
+    }
+    
+
+    func scrollViewDidEndDragging(scrollView: UIScrollView!,
+        willDecelerate decelerate: Bool) {
+        var offset = Float(abs(scrollView.contentOffset.y))
+        if offset > 30 {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+//    
+//    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
+//        // This method is called when the scrollview finally stops scrolling.
+//    }
+ 
     /*
     // MARK: - Navigation
 
